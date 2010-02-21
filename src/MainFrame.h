55efd921ec1ff00009499da9fb5b372b92f9eee0
Tobias Schaefer <tobiassch@users.sourceforge.net>
2010-02-21 15:49:35 +0000

    wxWidgets is set up. Program compiles under Linux. Shows a little window. So far, so good.
    git-svn-id: https://genericcam.svn.sourceforge.net/svnroot/genericcam/trunk@5 fa0ba55f-42f9-4c21-982c-c68e131b72ab

--- src/MainFrame.h
+++ src/MainFrame.h
@@ -12,38 +12,20 @@
 #include "StdInclude.h"
 #include "gui/gui.h"
 
-#include "GameEngine.h"
-
-class MainFrame:public MainFrameGui {
+class MainFrame:public GUIMainFrame {
 	// Constructor/ Destructor
 public:
 	MainFrame(wxWindow* parent);
 	virtual ~MainFrame();
 
-
 	// Member variables
 private:
-	float cogShift;
-
-	unsigned char takeFrom;
-
-	GameEngine engine;
-
 
 	// Methods
 private:
 
-//	void OnLoadImage(wxCommandEvent& event);
-	void OnPaint(wxPaintEvent& event);
 	void OnQuit(wxCommandEvent& event);
-	void OnNew(wxCommandEvent& event);
 	void OnAbout(wxCommandEvent& event);
-	void OnMotion(wxMouseEvent& event);
-	void OnLeftDown(wxMouseEvent& event);
-
-	void Animate(void);
-
-
 };
 
 #endif /* MAINFRAME_H_ */
